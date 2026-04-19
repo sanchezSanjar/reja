294cab1c98c88bcc2e8162779d6f49e94bac97a7
sanjar-nuraliev-student <sanjar@ajou.ac.kr>
2026-04-19 21:45:23 +0900

    initial commit

--- server.js
+++ server.js
@@ -15,12 +15,13 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-app.get("/hello", function (req, res) {
-  res.end(`<h1>HELLO WORLD </h1>`);
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
 
-app.get("/gift", function (req, res) {
-  res.end(`<h1>You are on gifts' page </h1>`);
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = http.createServer(app);
