329f57f3d4f9b24d7773d4f705d3c40cdf609db2
sanjar-nuraliev-student <sanjar@ajou.ac.kr>
2026-04-21 00:43:00 +0900

    feat: develop develop router ejs page

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -21,7 +32,10 @@ app.post("/create-item", (req, res) => {
   // res.json({ test: "success" });
 });
 
-app.get("/");
+app.get("/develop", (req, res) => {
+  res.render("develop", { user: user });
+});
+
 app.get("/", function (req, res) {
   res.render("harid");
 });
