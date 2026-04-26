00cbbfd7ec99da1dd26cdcf9386fb7e76bb05c23
sanjar-nuraliev-student <sanjar@ajou.ac.kr>
2026-04-27 00:01:22 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -1,28 +1,18 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
-const fs = require("fs");
-const path = require("path");
-
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
-  }
-});
 
 // 1: Kirish code
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 2: Session code
 
 // 3: Views code
-app.set("views", path.join(__dirname, "views"));
+app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
@@ -30,17 +20,15 @@ app.post("/create-item", (req, res) => {
   // TODO: code with DB here
 });
 
-app.get("/develop", (req, res) => {
-  res.render("develop", { user: user });
-});
-
 app.get("/", function (req, res) {
-  res.render("harid");
+  res.render("reja");
 });
 
 const server = http.createServer(app);
 let PORT = 3001;
 
 server.listen(PORT, function () {
-  console.log(`The server is running successfully on port: ${PORT}`);
+  console.log(
+    `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+  );
 });
